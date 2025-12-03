712d296dfa52e76d9be0fb07f564690941a847a7
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-03 18:09:14 +0100

    Fix Esercizi Bonus 1 e 5, ed esercizoi base 8

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,12 +10,20 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+
+//const reversedTeachers = teachers.reverse();
+const reversedTeachers = [];
+
+for( let i = teachers.length - 1; i >= 0; i--){
+  reversedTeachers.push(teachers[i]);
+}
 console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+// leggere l'array (for)
+// Decisione se maggiore o uguale a 5 caratteri (length)
 
 // 3. Rimuovi 'Ed' dall'array teachers
 const index = teachers.indexOf('Ed');
@@ -28,9 +36,18 @@ console.log(teachers);
 const isFabioPresent = teachers.includes('Fabio');
 console.log(isFabioPresent);
 
-// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-for (i = 0; i < teachers.length; i++){
-  let teachersString = `${teachers[i]},` ;
-  teachersList.innerHTML += teachersString;
+// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa separata da virgole e salvala nella variabile teachersString
+
+let teachersString = [];
+
+for ( let i = 0; i < teachers.length; i++){
+  if (i !== 5){
+  teachersString += `${teachers[i]},`;
+  }
+  else{
+    teachersString += teachers[i];
+  }
 }
+console.log(teachersString);
+teachersList.innerHTML += teachersString;
 
