6a77bb47cb7177c2dc6988fba4b0e5abdef48162
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-03 18:42:47 +0100

    fix Bonus esercizio 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -23,11 +23,11 @@ console.log(reversedTeachers);
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 
-let longNames = [];
+const longNames = [];
 
 for (let i = 0; i < teachers.length; i++){
   if (teachers[i].length >= 5){
-    longNames += `${teachers[i]} `;
+    longNames.push(`${teachers[i]}`);
   }
 }
  console.log(longNames);
